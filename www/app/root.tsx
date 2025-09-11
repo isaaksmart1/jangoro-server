3e64997a1dd867d4e9f8340e43d32d49dde99fb0
isaaksmartstubbenedge <isaaks@stubbenedge.com>
2025-09-11 15:16:17 +0100

    Adde google tag.

--- www/app/root.tsx
+++ www/app/root.tsx
@@ -76,6 +76,16 @@ export default function App() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T6KHNWCM');`,
+          }}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             function gtag_report_conversion_free(url) {
               var callback = function () {
                 if (typeof(url) != 'undefined') {
@@ -112,7 +122,7 @@ export default function App() {
             }`,
           }}
         ></script>
-                <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
             function gtag_report_conversion_year(url) {
@@ -171,7 +181,14 @@ export default function App() {
         />
       </head>
       <body className="h-full">
-        <noscript></noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T6KHNWCM"
+            height={0}
+            width={0}
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
